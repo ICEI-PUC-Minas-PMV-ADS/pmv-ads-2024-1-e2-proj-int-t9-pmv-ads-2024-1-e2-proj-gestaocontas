54ddf36aab9b4d72cda6470290b43f3fdc50a28c
Rodrigo Araujo Ribeiro <144185306+1806rodrigo@users.noreply.github.com>
2024-06-05 11:12:02 -0300

    Update User.cs
    alterado, (atentar a Table Users e class User
    pois quando gerei o programa utilizer Usuarios e Usuario

--- src/Trabalho_PUC/Models/User.cs
+++ src/Trabalho_PUC/Models/User.cs
@@ -1,30 +1,37 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.Build.Graph;
+using Xunit.Sdk;
 
 namespace Trabalho_PUC.Models
 {
-   
-        [Table("Users")]
-        public class User
-        {
-            [Key]
-            public int Id { get; set; }
 
-            [Required(ErrorMessage = "Obrigatório informar o nome")]
-            public  string Nome { get; set; }
+    [Table("Users")]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o nome")]
+        public string Nome { get; set; }
 
 
-            [Required(ErrorMessage = "Obrigatório informar o senha")]
-            [DataType(DataType.Password)]
-            public  string Senha { get; set; }
+        [Required(ErrorMessage = "Obrigatório informar o senha")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
 
-            [Required(ErrorMessage = "Obrigatório informar o senha")]
-            public Perfil Perfil { get; set; }
-        }
-        public enum Perfil
-        {
-            Admin,
-            User
-        }
-   
+        [Required(ErrorMessage = "Obrigatório informar o senha")]
+        public Perfil Perfil { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d{3}\.\d{3}\.\d{3}\-\d{2}$", ErrorMessage = "CPF inválido (digite o CPF da seguinte maneira xxx.xxx.xxx-xx)")]
+        public string CPF { get; set; }
+
+
+    }
+    public enum Perfil
+    {
+        Admin,
+        User
+    }
 }
